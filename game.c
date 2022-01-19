4c30e17a10a9abf2cbcde2826361a0895dddff5e
Stephen Carder <stephen@stafngrimr.com>
2022-01-19 14:51:36 +0000

    Making new repo. Add 2 new C practice pages

---
+++ game.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+
+    char *intro = "You are an adventurer waiting togo out into the big wide world where you will be encountering foes magic and all sorts of wonders.";
+
+    char *intro2 = "You have got to make sure that your bag is both packed and ready to store the things you find along the way.";
+
+    printf("%s\n%s\n", intro, intro2);
+}
