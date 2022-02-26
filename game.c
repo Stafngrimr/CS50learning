3d3da449c628b11791e0f3a2df0cf9f81bb0f362
Stephen Carder <stephen@stafngrimr.com>
2022-02-26 16:43:49 +0000

    Updated packcontents & questions, moved choice up to main

--- game.c
+++ game.c
@@ -7,97 +7,103 @@
   user would like to add anything to the pack or keep listing it's contents. Only exits
   when prompted.
 
-  :ideas:
+  :IDEAS:
   -Could add a feature where you actually pick from a pool of random objects to add
   -first 3 items are picked randomly from list of 30 each time
-  -A Go forwards feature? Then items are randomly come across
+  -A Go forwards feature? Then items are randomly come across and you choose whether
+  or not to add them to your pack.
 
 */
 
+// Include relevant libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
 
+// Prototyping functions & global variables
 int choose();
-// TODO: Turn listing of pack contents into it's own function
-// TODO: Turn adding to pack into it's own function
-
+void packContents();
+void questions();
+// TODO: Turn adding to pack into a function
 
 char *item1 = "One hunk of Alterac Swiss";
 char *item2 = "Two Mana potions";
 char *item3 = "A Vanklet's studded belt";
-int choice;
+int choice = 0;
 
-// TODO : Don't forget to take this random comment out
 
 int main(void)
 {
-  // Intro speech & contents of pack
+  // Intro
   printf("\n");
   printf(" You are an adventurer with a pack on your back, ready to explore the world!\n");
-  printf("\n");
-  printf(" So far in your pack you have:\n");
-  printf("  %s, %s, and %s.\n\n", item1, item2, item3);
+  questions();
 
-  // Loop for Questions & Answers
+  // Loop answers until exit
   do
   {
-    choice = choose();
+    scanf("%i", &choice);
 
+    // 1 - Check the contents of your pack
     if (choice == 1)
     {
+      packContents();
       printf("\n");
-      printf(" You're relisting what's in your pack!\n");
-      printf("\n");
-      // Eventually just : packContents()
     }
 
+    // 2 - Add a found item to your pack
     else if (choice == 2)
     {
       printf("\n");
-      printf(" You're going to add to your pack!\n");
+      printf(" You'd like to add to your pack.. WELL YOU CAN'T!!\n");
       printf("\n");
-      // Eventually just : addContents()
+      // TODO: Eventually just addContents()
     }
 
-    else
+    // 3 - What are the questions again?
+    else if (choice == 3)
+    {
+      questions();
+      printf("\n");
+    }
+
+    // 4 - Quit the game
+    else if (choice == 4)
     {
       printf("\n");
       printf("Well, okay then. See ya later amigo.\n");
       printf("\n");
       return 0;
     }
-  }
-  while (choice != 0);
-}
-
-// Game asks player what to do next and gives 3 options, returns int
-// BUG: Anything but an integer causes infinite loop of Stupid string
-int choose()
-{
-  printf(" What would you like to do next?\n");
-  printf("   1 - Check the contents of your pack\n");
-  printf("   2 - Add a found item to your pack\n");
-  printf("   3 - Quit the game\n\n");
-
-  int *path = malloc(sizeof(int));
-
-  do
-  {
-    scanf("%i", path);
-
-    if ((*path == 1) || (*path == 2) || (*path == 3))
-    {
-      return *path;
-      free(path);
-    }
 
     else
     {
       printf("\n");
-      printf(" What are you, stupid? Just a 1 or a 2 or a 3 will do..\n");
+      printf("That's not one of the options dumb-dumb. You lose.\n");
+      printf("\n");
+      return 1;
     }
   }
-  while (*path != 1 || *path != 2 || *path !=3);
+
+  while (choice != 1 || choice != 2 || choice !=3 || choice !=4);
+}
+
+// Function: listing of the current pack contents
+void packContents()
+{
+  printf("\n");
+  printf(" So far in your pack you have:\n");
+  printf("  %s, %s, and %s.\n", item1, item2, item3);
+}
+
+// Function: Listing of the questions
+void questions()
+{
+  printf("\n");
+  printf(" What would you like to do next?\n");
+  printf("   1 - Check the contents of your pack\n");
+  printf("   2 - Add a found item to your pack\n");
+  printf("   3 - What are the questions again?\n");
+  printf("   4 - Quit the game\n\n");
 }
